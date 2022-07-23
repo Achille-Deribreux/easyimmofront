f007d2488df9878699587eb76eb7451897fb596a
« Achille <achille@deribreux.be>
2022-07-23 23:18:14 +0200

    feat(deployment)/EAS-25 hotfix app name for deployment

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<easyimmofront>'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/<app-name>/index.html'));});
+    '/dist/<easyimmofront>/index.html'));});
 app.listen(process.env.PORT || 8080);
