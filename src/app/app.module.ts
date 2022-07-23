8c6286554ca6dfb6780b1c98990a561cc6baa8d7
Achille Deribreux <achille@deribreux.be>
2022-07-23 20:18:03 +0200

    feat(all)-EAS-16 add basic components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PropertyComponent } from './property/components/property/property.component';
+import { PropertyListComponent } from './property/components/property-list/property-list.component';
+import { IncomeComponent } from './income/components/income/income.component';
+import { IncomeListComponent } from './income/components/income-list/income-list.component';
+import { FeeComponent } from './fee/components/fee/fee.component';
+import { FeeListComponent } from './fee/components/fee-list/fee-list.component';
+import { ReservationComponent } from './reservation/components/reservation/reservation.component';
+import { ReservationListComponent } from './reservation/components/reservation-list/reservation-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PropertyComponent,
+    PropertyListComponent,
+    IncomeComponent,
+    IncomeListComponent,
+    FeeComponent,
+    FeeListComponent,
+    ReservationComponent,
+    ReservationListComponent
   ],
   imports: [
     BrowserModule,
