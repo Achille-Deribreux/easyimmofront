a6bc235cb1f367c7c3725af2103f1d9ec83a5f51
« Achille <achille@deribreux.be>
2022-07-29 00:02:58 +0200

    upd(income)/EAS-24 add income table

--- src/app/income/income.service.ts
+++ src/app/income/income.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Income} from "./income.model";
+import {IncomeSummary} from "./models/income-summary.model";
 
 @Injectable({
   providedIn: 'root'
@@ -22,9 +23,14 @@ export class IncomeService {
 
   demoIncomes: Income[] = [this.demoIncome1, this.demoIncome2, this.demoIncome3, this.demoIncome4, this.demoIncome5, this.demoIncome6, this.demoIncome7, this.demoIncome8, this.demoIncome9];
 
-  getAllIncomes() : Income[] {
+  demoIncomeSummary1 : IncomeSummary = new IncomeSummary(1, new Date("2020-01-01"),1341,"Income 1 for property 1" );
+  demoIncomeSummary2: IncomeSummary = new IncomeSummary(2, new Date("2020-01-02"),453,"Income 2 for property 1");
+  demoIncomeSummary3: IncomeSummary = new IncomeSummary(3, new Date("2020-01-01"),123,"Income 1 for property 1", );
+  demoIncomeSummaries: IncomeSummary[] = [this.demoIncomeSummary1, this.demoIncomeSummary2, this.demoIncomeSummary3];
+
+  getAllIncomes() : IncomeSummary[] {
     //TODO: http call to get all properties
-    return this.demoIncomes;
+    return this.demoIncomeSummaries;
   }
 
   getIncomesForProperty(id: number) : Income[] {
