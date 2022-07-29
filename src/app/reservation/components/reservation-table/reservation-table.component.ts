3b55a5f3c5a17801dbd17acac3113dffb6dfba8a
« Achille <achille@deribreux.be>
2022-07-29 14:18:09 +0200

    refactor(fee)/EAS-33 add reservation-table compo

---
+++ src/app/reservation/components/reservation-table/reservation-table.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input} from '@angular/core';
+import {ReservationSummary} from "../../model/reservation-summary.model";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-reservation-table',
+  templateUrl: './reservation-table.component.html',
+  styleUrls: ['./reservation-table.component.scss']
+})
+export class ReservationTableComponent {
+
+  @Input() reservationList! : ReservationSummary[];
+  @Input() displayedColumns! : string[];
+
+  constructor(private router : Router) { }
+
+
+  showReservation(id:number) {
+    this.router.navigateByUrl('/reservation/'+id);
+  }
+
+}
