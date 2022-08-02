5f74dfb22e4eda61498f3f0540d039255823f108
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-08-02 12:00:38 +0200

    feat(income)/EAS-42 http call to get all properties

--- src/app/income/income.service.ts
+++ src/app/income/income.service.ts
@@ -3,6 +3,7 @@ import {IncomeSummary} from "./models/income-summary.model";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {IncomeBody} from "./models/income-body.model";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,8 @@ export class IncomeService {
 
   constructor(private router : Router, private http: HttpClient) { }
 
-  demoIncomeSummary1 : IncomeSummary = new IncomeSummary(1, new Date("2020-01-01"),1341,"Income 1 for property 1");
-  demoIncomeSummary2: IncomeSummary = new IncomeSummary(2, new Date("2020-01-02"),453,"Income 2 for property 1");
-  demoIncomeSummary3: IncomeSummary = new IncomeSummary(3, new Date("2020-01-01"),123,"Income 1 for property 1");
-  demoIncomeSummaries: IncomeSummary[] = [this.demoIncomeSummary1, this.demoIncomeSummary2, this.demoIncomeSummary3];
-
-  getAllIncomes() : IncomeSummary[] {
-    //TODO: http call to get all properties
-    return this.demoIncomeSummaries;
+  getAllIncomes(): Observable<IncomeSummary[]> {
+    return this.http.get<IncomeSummary[]>('https://easy-immo-back.herokuapp.com/income/getAll');
   }
 
   showIncome(id:number) {
@@ -26,8 +21,6 @@ export class IncomeService {
   }
 
   addIncome(income: IncomeBody) {
-    this.http
-      .post('https://easy-immo-back.herokuapp.com/income/add', income)
-      .subscribe();
+    this.http.post('https://easy-immo-back.herokuapp.com/income/add', income).subscribe();
   }
 }
