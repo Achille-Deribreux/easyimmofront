22a0240373a301d487a0fb11a8ad282b5cd1b696
Achille Deribreux <achille@deribreux.be>
2022-08-07 17:16:42 +0200

    feat(all)/EAS-43 fix url

--- src/app/fee/fee.service.ts
+++ src/app/fee/fee.service.ts
@@ -17,22 +17,22 @@ export class FeeService {
   backDevHost = "http://localhost:8080/";
 
   getAllFees() : Observable<FeeSummary[]> {
-    return this.http.get<FeeSummary[]>("https://easy-immo-back.herokuapp.com/fee/getAll");
+    return this.http.get<FeeSummary[]>(this.backBaseHost+"fee/getAll");
   }
 
   getFee(id:number) : Observable<FeeDetail> {
     const params = new HttpParams()
       .set('id', id);
-    return this.http.get<FeeDetail>(this.backDevHost+"fee/getById", {params});
+    return this.http.get<FeeDetail>(this.backBaseHost+"fee/getById", {params});
   }
 
   addFee(fee : FeeBody) : Observable<FeeBody> {
-    return this.http.post<FeeBody>(this.backDevHost+"fee/add", fee);
+    return this.http.post<FeeBody>(this.backBaseHost+"fee/add", fee);
   }
 
   editFee(fee: FeeBody, id : number) : Observable<FeeBody> {
     const params = new HttpParams()
       .set('id', id);
-    return this.http.put<FeeBody>(this.backDevHost+"fee/update",fee, {params});
+    return this.http.put<FeeBody>(this.backBaseHost+"fee/update",fee, {params});
   }
 }
