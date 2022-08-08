96514261e9bdcc0ed62ed8146a3ed4b4fc75f0ec
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-08-08 23:00:20 +0200

    feat(fee)/EAS-49 page to display the details of a fee

--- src/app/fee/components/fee/fee.component.ts
+++ src/app/fee/components/fee/fee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FeeService} from "../../fee.service";
+import {FeeDetail} from "../../model/fee-detail";
+import {ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-fee',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeeComponent implements OnInit {
 
-  constructor() { }
+  fee!: FeeDetail;
+  feeId!: number;
+
+  constructor(private feeService: FeeService, private route: ActivatedRoute, private router: Router ) { }
 
   ngOnInit(): void {
+    this.feeService.getFee(this.route.snapshot.params['id']).subscribe({
+      next: (fee: FeeDetail) => {
+        this.fee = fee;
+    },
+    error: (err) => {console.log(err);}
+    });
+
+    this.feeId = this.route.snapshot.params['id'];
+  }
+
+  redirectToProperty():void {
+    this.router.navigateByUrl('property/'+this.fee.property.id);
   }
 
 }
