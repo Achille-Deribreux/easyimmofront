412c0f97c0b8bb3fdfdf8bce96c14042fbde07a7
Achille Deribreux <achille@deribreux.be>
2022-08-28 15:32:50 +0200

    feat(register)/EAS-56 register comp

---
+++ src/app/user/login/components/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl} from "@angular/forms";
+import {LoginService} from "../../login.service";
+import {User} from "../../models/user-model";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  formValues = this.formBuilder.group({
+    username:new FormControl(''),
+    password:new FormControl(''),
+  })
+
+  onSubmit() {
+    //let user : User = new User(this.formValues.value.username, bcrypt.hashSync(this.formValues.value.password, 10));
+    let user : User = new User(this.formValues.value.username, this.formValues.value.password);
+    this.loginService.register(user);
+  }
+
+}
