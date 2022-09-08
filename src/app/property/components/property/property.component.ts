d5a1f4b5236a199c0328e16007ad5842afa60007
Guillaume De Backre <debackre.guillaume@gmail.com>
2022-09-08 10:55:01 +0200

    feat(property)/EAS-54 delete method with refresh after execution

--- src/app/property/components/property/property.component.ts
+++ src/app/property/components/property/property.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {PropertyService} from "../../property.service";
 import {PropertyDetails} from "../../models/property-details.model";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-property',
@@ -11,12 +11,13 @@ import {ActivatedRoute} from "@angular/router";
 export class PropertyComponent implements OnInit {
 
   property!: PropertyDetails;
+  propertyId!: number;
 
   displayedIncomeColumns: string[] = ['date','description',"amount","button"];
   displayedFeesColumns: string[] = ['date','supplier',"amount","button"];
   displayedReservationColumns: string[] = ['fromDate','toDate',"price","button"];
 
-  constructor(private propertyService: PropertyService, private route : ActivatedRoute) { }
+  constructor(private propertyService: PropertyService, private route : ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.propertyService.getProperty(this.route.snapshot.params['id']).subscribe({
@@ -25,10 +26,19 @@ export class PropertyComponent implements OnInit {
       },
      error: (err) => {console.log(err);}
     });
+
+    this.propertyId = this.route.snapshot.params['id'];
+
+    this.propertyService.RefreshRequired.subscribe()
   }
 
   getBankLoanPercentage(refundedAmount: number, totalAmount : number): number {
     return (refundedAmount/totalAmount)*100;
   }
 
+  deleteProperty(propertyId: number): void {
+    this.propertyId = propertyId;
+    this.propertyService.deleteProperty(propertyId).subscribe();
+    this.router.navigateByUrl('properties');
+  }
 }
--- src/app/property/property.service.ts
+++ src/app/property/property.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {PropertySummary} from "./models/property-summary.model";
 import {PropertyDetails} from "./models/property-details.model";
-import {Observable} from "rxjs";
+import {Observable, Subject, tap} from "rxjs";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {PropertyBody} from "./models/property-body.model";
 
@@ -15,6 +15,12 @@ export class PropertyService {
 
   backBaseHost = "https://easy-immo-back.herokuapp.com/";
   backDevHost = "http://localhost:8080/";
+
+  private _refreshRequired = new Subject<void>();
+
+  get RefreshRequired(){
+    return this._refreshRequired;
+  }
 
   getAllProperties() : Observable<PropertySummary[]> {
     return this.http.get<PropertySummary[]>(this.backBaseHost+"property/getAll");
@@ -32,5 +38,15 @@ export class PropertyService {
     const params = new HttpParams()
       .set('id', id);
     return this.http.put<PropertyBody>(this.backBaseHost+"property/update",property, {params});
+  }
+
+  deleteProperty(id : number) {
+    const params = new HttpParams()
+      .set('id', id);
+    return this.http.delete(this.backBaseHost+"property/deleteById", {params}).pipe(
+      tap(() => {
+        this.RefreshRequired.next();
+      })
+    );
   }
 }
